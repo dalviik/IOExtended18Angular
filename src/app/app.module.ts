072c70521cef7ce268a06121c2c05066182261f4
dalviik <duglasapazaa@gmail.com>
2018-08-06 12:03:30 -0400

    activities, schedule, register, sponsors, footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { IntroComponent } from './intro/intro.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { SpeakersComponent } from './speakers/speakers.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { RegisterComponent } from './register/register.component';
+import { SponsorsComponent } from './sponsors/sponsors.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    IntroComponent,
+    ActivitiesComponent,
+    SpeakersComponent,
+    ScheduleComponent,
+    RegisterComponent,
+    SponsorsComponent
   ],
   imports: [
     BrowserModule
